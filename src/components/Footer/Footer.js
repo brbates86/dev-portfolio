ab6ebcc84d5c1f3d52e658528382a7c5c3af8dc8
Brett Bates <brettbates86@gmail.com>
2023-02-25 09:55:13 -0600

    code for footer.js and css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/brbates86'
+      className='link footer__link'
+    >
+      Created By brb coding & web developement
+    </a>
+  </footer>
+)
+
+export default Foot
